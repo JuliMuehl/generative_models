a919e56d774df0b624431ae02528fba65224ea82
Julian Mühlberger <julian.muehlberger@t-online.de>
2026-06-02 18:17:09 +0200

    Split plenoxels rendering into abstract base class

--- src/generative_models/plenoxels/models.py
+++ src/generative_models/plenoxels/models.py
@@ -2,22 +2,54 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 import os
+from abc import ABC, abstractmethod
 
-class DiffuseVoxelGrid(torch.nn.Module):
-    def __init__(self, N, tv_loss_weight=1e-3, sparsity_loss_weight = 1.0, bg_rgb = [1.0, 1.0, 1.0], softplus_beta = 1e1):
+class DifferentiableRenderer(torch.nn.Module, ABC):
+    def __init__(self, bg_rgb = [1.0, 1.0, 1.0]):
         super().__init__()
+        self.register_buffer("bg_rgb", torch.tensor(bg_rgb, dtype=torch.float))
+
+    @abstractmethod
+    def sample(self, xzy, dirs):
+        pass
+
+    @abstractmethod
+    def tv_loss(self):
+        pass
+
+    def forward(self, ray_origins, ray_directions, targets=None, num_samples=32, calc_tv_loss = False):
+        device = next(self.parameters()).device
+        t = torch.linspace(0, 2.0, num_samples).to(device)
+        # [num_rays , num_samples , 3]
+        xyz = ray_origins[:, None, :] + t[None, :, None] * ray_directions[:, None, :]
+        colors, densities = self.sample(xyz, ray_directions)
+        alpha = 1.0 - torch.exp(-densities*(t[1] - t[0]))
+        transmittance = torch.cumprod(1.0 - alpha, dim=1)
+        weights = alpha * transmittance
+        background = transmittance[:, -1:, 0] * self.bg_rgb[None, :]
+        radiance = torch.sum(weights * colors, dim=1) + background
+        if targets is not None:
+            sparsity_loss = torch.mean(torch.log(1 + 4 * alpha * (1 - alpha)))
+            loss = F.mse_loss(radiance,targets) + self.sparsity_loss_weight * sparsity_loss
+            if calc_tv_loss:
+                loss += self.tv_loss_weight * self.tv_loss()
+            return radiance, loss
+        return radiance
+    
+class DiffuseVoxelGrid(DifferentiableRenderer):
+    def __init__(self, N, tv_loss_weight=1e-3, sparsity_loss_weight = 1.0, bg_rgb = [1.0, 1.0, 1.0], softplus_beta = 1e1):
+        super().__init__(bg_rgb=bg_rgb)
         self.N = N
         self.color_grid = torch.nn.Parameter(0.2 * torch.randn(1, 3, N, N, N))
         self.density_grid = torch.nn.Parameter(-1.0 * torch.ones(1, 1, N, N, N))
         self.tv_loss_weight = tv_loss_weight
         self.sparsity_loss_weight = sparsity_loss_weight
         self.softplus_beta = softplus_beta
-        self.register_buffer("bg_rgb", torch.tensor(bg_rgb, dtype=torch.float))
 
     def softplus(self, x):
         return F.softplus(self.softplus_beta * x) / self.softplus_beta
 
-    def sample(self, xyz):
+    def sample(self, xyz, dirs=None):
         n = 1
         shape = xyz.shape
         for d in xyz.shape[:-1]:
@@ -38,24 +70,6 @@ class DiffuseVoxelGrid(torch.nn.Module):
         tv_density = torch.mean(torch.sqrt(density_dxsq + density_dysq + density_dzsq + 1e-5))
         return  (tv_color + tv_density)
 
-    def forward(self, ray_origins, ray_directions, targets=None, num_samples=32, calc_tv_loss = False):
-        device = next(self.parameters()).device
-        t = torch.linspace(0, 2.0, num_samples).to(device)
-        xyz = ray_origins[:, None, :] + t[None, :, None] * ray_directions[:, None, :]
-        colors, densities = self.sample(xyz)
-        alpha = 1.0 - torch.exp(-densities*(t[1] - t[0]))
-        transmittance = torch.cumprod(1.0 - alpha, dim=1) 
-        weights = alpha * transmittance
-        background = transmittance[:, -1:, 0] * self.bg_rgb[None, :]
-        radiance = torch.sum(weights * colors, dim=1) + background
-        if targets is not None:
-            sparsity_loss = torch.mean(torch.log(1 + 4 * alpha * (1 - alpha)))
-            loss = F.mse_loss(radiance,targets) + self.sparsity_loss_weight * sparsity_loss
-            if calc_tv_loss:
-                loss += self.tv_loss_weight * self.tv_loss()
-            return radiance, loss
-        return radiance 
-
     def save_numpy(self):
         color_grid = F.sigmoid(self.color_grid).view(3, self.N, self.N, self.N).permute(1,2,3,0).detach().to("cpu").numpy()
         density_grid = self.softplus(self.density_grid).view(1, self.N, self.N, self.N).permute(1, 2, 3, 0).detach().to("cpu").numpy()
@@ -63,9 +77,9 @@ class DiffuseVoxelGrid(torch.nn.Module):
         os.replace("voxel_data.part.npz", "voxel_data.npz")
 
 
-class SH2VoxelGrid(torch.nn.Module):
+class SH2VoxelGrid(DifferentiableRenderer):
     def __init__(self, N, tv_loss_weight=1e-3, sparsity_loss_weight = 1.0, bg_rgb = [1.0, 1.0, 1.0], softplus_beta = 1e1, mode="plenoptic"):
-        super().__init__()
+        super().__init__(bg_rgb=bg_rgb)
         self.N = N
         self.density_grid = torch.nn.Parameter(-1.0 * torch.ones(1, 1, N, N, N))
         pi = np.pi
@@ -134,25 +148,6 @@ class SH2VoxelGrid(torch.nn.Module):
         density_dzsq = (self.density_grid[:, :, :-1, :-1, :-1] - self.density_grid[:, :, :-1, :-1,  1:])**2
         tv_density = torch.mean(torch.sqrt(density_dxsq + density_dysq + density_dzsq + 1e-5))
         return  (tv_sh2 + tv_density)
-
-    def forward(self, ray_origins, ray_directions, targets=None, num_samples=32, calc_tv_loss = False):
-        device = next(self.parameters()).device
-        t = torch.linspace(0, 2.0, num_samples).to(device)
-        # [num_rays , num_samples , 3]
-        xyz = ray_origins[:, None, :] + t[None, :, None] * ray_directions[:, None, :]
-        colors, densities = self.sample(xyz, ray_directions)
-        alpha = 1.0 - torch.exp(-densities*(t[1] - t[0]))
-        transmittance = torch.cumprod(1.0 - alpha, dim=1)
-        weights = alpha * transmittance
-        background = transmittance[:, -1:, 0] * self.bg_rgb[None, :]
-        radiance = torch.sum(weights * colors, dim=1) + background
-        if targets is not None:
-            sparsity_loss = torch.mean(torch.log(1 + 4 * alpha * (1 - alpha)))
-            loss = F.mse_loss(radiance,targets) + self.sparsity_loss_weight * sparsity_loss
-            if calc_tv_loss:
-                loss += self.tv_loss_weight * self.tv_loss()
-            return radiance, loss
-        return radiance
 
     def save_numpy(self):
         sh2_grid = self.sh2_grid.view(3, 9, self.N, self.N, self.N).permute(1,2,3,4,0).detach().to("cpu").numpy()
